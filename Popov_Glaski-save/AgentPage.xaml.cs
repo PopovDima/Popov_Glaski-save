128b4d122fdb28a7c1d04705efde4c031673202a
Dima <legdim17@gmail.com>
2026-02-16 13:06:39 +0500

    lab 17

--- Popov_Glaski-save/AgentPage.xaml.cs
+++ Popov_Glaski-save/AgentPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Popov_Glaski_save.database;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +23,92 @@ namespace Popov_Glaski_save
     /// </summary>
     public partial class AgentPage : Page
     {
+        private Popov_GlaskiSaveEntities _context;
         public AgentPage()
         {
             InitializeComponent();
+
+            _context = Popov_GlaskiSaveEntities.GetContext();
+
+            CBoxFilter.ItemsSource = _context.AgentType.Select(at => new {
+                Id = at.ID,
+                Name = at.Title
+            }).Prepend(new {Id = -1, Name = "Все типы"}).ToList();
+            CBoxFilter.SelectedValuePath = "Id";
+            CBoxFilter.DisplayMemberPath = "Name";
+
+            CBoxFilter.SelectedIndex = 0;
+
+            UpdateAgents();
+        }
+
+        private void UpdateAgents()
+        {
+            var currentAgents = _context.Agent.ToList();
+
+            string searchText = TBoxSearch.Text.ToLower();
+            string clearSearchText = new string(searchText.Where(char.IsDigit).ToArray());
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                currentAgents = currentAgents.Where(a =>
+                    a.Title.ToLower().Contains(searchText) ||
+                    a.Email.ToLower().Contains(searchText) ||
+                    (!string.IsNullOrEmpty(clearSearchText) && a.ClearPhone.Contains(clearSearchText))
+                ).ToList();
+            }
+
+            if (CBoxFilter.SelectedValue is int selectedId && selectedId != -1)
+            {
+                currentAgents = currentAgents.Where(a => a.AgentTypeID == selectedId).ToList();
+            }
+
+            if (CBoxSort.SelectedItem != null)
+            {
+                switch (CBoxSort.SelectedIndex)
+                {
+                    case 0:
+                        currentAgents = currentAgents.OrderBy(a => a.Title).ToList();
+                        break;
+                    case 1:
+                        currentAgents = currentAgents.OrderByDescending(a => a.Title).ToList();
+                        break;
+                    case 3:
+                        currentAgents = currentAgents.OrderBy(a => a.Discount).ToList();
+                        break;
+                    case 4:
+                        currentAgents = currentAgents.OrderByDescending(a => a.Discount).ToList();
+                        break;
+                    case 6:
+                        currentAgents = currentAgents.OrderBy(a => a.Priority).ToList();
+                        break;
+                    case 7:
+                        currentAgents = currentAgents.OrderByDescending(a => a.Priority).ToList();
+                        break;
+                }
+            }
+
+            ListViewAgents.ItemsSource = currentAgents;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditAgentPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void CBoxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
+        }
+
+        private void CBoxFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateAgents();
         }
     }
 }
